ba96bbdf5b52eb8a5aa485bb57855b5cb0d2ed4c
isabuky <isaac.sande@gmail.com>
2020-10-21 23:22:07 +0200

    ejercicio 2 de la unidad 2 resuelto

---
+++ Programacion_FP_Distancia/src/unidad2/Conversor.java
@@ -0,0 +1,33 @@
+package unidad2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.DecimalFormat;
+
+public class Conversor {
+
+	public static void main(String[] args) throws IOException { 
+		
+		final double ValorDolar = 1.19;
+		
+		double dolares;
+		
+		BufferedReader euros = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.print("Introduzca en nº de Euros que desea convertir: ");
+		
+		String n = euros.readLine();
+		
+		double RealN = Double.parseDouble(n); //Esto convertirá Java String a java double
+		
+		dolares = RealN*ValorDolar;
+		
+		DecimalFormat DosDecimales = new DecimalFormat("#.##"); //Instrucción para redondear decimales
+					
+		System.out.println(DosDecimales.format (dolares) + " Dolares.");
+		
+		
+	}
+
+}
