0c3779832317287bfa7b4d8d43dcb2eef790d326
isabuky <isaac.sande@gmail.com>
2020-10-22 23:30:01 +0200

    Ejercicio 4 de la unidad 2 resuelto

--- Programacion_FP_Distancia/src/unidad2/Calificaciones.java
+++ Programacion_FP_Distancia/src/unidad2/Calificaciones.java
@@ -9,6 +9,8 @@ public class Calificaciones {
 
 	public static void main(String[] args) throws IOException {
 						
+			//MATEMÁTICAS
+		
 			BufferedReader t1m = new BufferedReader(new InputStreamReader(System.in));
 			
 			System.out.print("Nota de la Tarea 1 de matemáticas: ");
@@ -48,27 +50,91 @@ public class Calificaciones {
 			DecimalFormat fm = new DecimalFormat("#.00");
 			
 			System.out.println("La calificación de la asignatura de matemáticas es " + fm.format(matemáticas)); 
-		
+				
+			//FÍSICA		
 			
-		
-		
-			
-			
-			
-			
-			
-			//float tfis = (t1fis, t2fis, t3fis / 3);
-			
-			//float física = ((exfis * 0.9) + (tfis * 0.1)); //nota física
+				BufferedReader t1f = new BufferedReader(new InputStreamReader(System.in));
 					
-			//float tqui = (t1qui, t2qui, t3qui / 3);
-			
-			//float química = ((exqui * 0.9) + (tqui * 0.1)); //nota química
-			
-		
-		
-		
-
+				System.out.print("Nota de la Tarea 1 de física: ");
+					
+				String t1fa = t1f.readLine();
+					
+				float t1fat = Float.parseFloat(t1fa);
+					
+				BufferedReader t2f = new BufferedReader(new InputStreamReader(System.in));
+					
+				System.out.print("Nota de la Tarea 2 de física: ");
+					
+				String t2fa = t2f.readLine();
+					
+				float t2fat = Float.parseFloat(t2fa);
+					
+				BufferedReader t3f = new BufferedReader(new InputStreamReader(System.in));
+										
+				float tfis = (t1fat + t2fat) / 2;
+					
+				BufferedReader exf = new BufferedReader(new InputStreamReader(System.in));
+					
+				System.out.print("Nota del exámen física: ");
+					
+				String exfa = exf.readLine();
+					
+				float exfat = Float.parseFloat(exfa);
+					
+				double física = (exfat * 0.8 + tfis * 0.2);
+					
+				DecimalFormat ff = new DecimalFormat("#.00");
+						
+				System.out.println("La calificación de la asignatura de física es " + ff.format(física));
+					
+				//QUÍMICA			
+				
+				BufferedReader t1q = new BufferedReader(new InputStreamReader(System.in));
+					
+				System.out.print("Nota de la Tarea 1 de química: ");
+				
+				String t1qa = t1q.readLine();
+					
+				float t1qat = Float.parseFloat(t1qa);
+					
+				BufferedReader t2q = new BufferedReader(new InputStreamReader(System.in));
+					
+				System.out.print("Nota de la Tarea 2 de química: ");
+					
+				String t2qa = t2q.readLine();
+					
+				float t2qat = Float.parseFloat(t2qa);
+					
+				BufferedReader t3q = new BufferedReader(new InputStreamReader(System.in));
+					
+				System.out.print("Nota de la Tarea 3 de química: ");
+					
+				String t3qa = t3q.readLine();
+					
+				float t3qat = Float.parseFloat(t3qa);
+				
+				float tqui = (t1qat + t2qat + t3qat) / 3;
+					
+				BufferedReader exq = new BufferedReader(new InputStreamReader(System.in));
+					
+				System.out.print("Nota del exámen física: ");
+					
+				String exqa = exq.readLine();
+					
+				float exqat = Float.parseFloat(exqa);
+					
+				double química = (exqat * 0.85 + tqui * 0.15);
+					
+				DecimalFormat fq = new DecimalFormat("#.00");
+						
+				System.out.println("La calificación de la asignatura de química es " + fq.format(química));
+				
+				//NOTA MEDIA
+				
+				DecimalFormat fmed = new DecimalFormat("#.00");
+								
+				System.out.println("La nota media del curso es de " + fmed.format((matemáticas + física + química) / 3));
+				
 	}
 
 }
